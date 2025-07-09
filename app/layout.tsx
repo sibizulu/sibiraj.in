21f5fa2638deb1c5b754f8ac397ffc4cc6cd5b38
Sibiraj PR <sibiraj@lightrains.com>
2025-07-09 13:39:02 +0530

    Updating font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,32 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Lora } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Manrope, Inter } from 'next/font/google'
+import './globals.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const manrope = Manrope({
+  variable: '--font-manrope',
+  subsets: ['latin']
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const lora = Lora({
-  variable: "--font-lora",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "Sibiraj PR",
-  description: "Work in progress, just like this page.",
-};
+  title: 'Sibiraj PR',
+  description: 'Work in progress, just like this page.'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${lora.variable} antialiased`}
-      >
+      <body className={`${manrope.variable} ${inter.variable} antialiased`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
